624218c4d1a876dc2a446355862d8ba43abae4d7
kc-leung <chiu-leung@Hotmail.com>
2019-09-16 17:31:12 -0400

    partial location dataModal update

---
+++ models/location.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// create Location Schema & model
+const LocationSchema = new Schema({
+  locationId: {
+    type: Schema.Types.ObjectId,
+    ref: "mark",
+    required: [true, "locationId is required to save Location"]
+  },
+  groupId: {
+    type: Schema.Types.ObjectId,
+    ref: "group",
+    required: [true, "groupId required to save Location"]
+  },
+  locationName: {
+    type: String,
+    required: [true, "locationName field is required"]
+  },
+  locationAddress: {
+    type: String,
+    required: [true, "locationAddress (string) is required"]
+  },
+  priceRange: {
+    type: String,
+    required: [true, "priceRange ($, $$, $$$) is required"],
+    validate: {
+      validator: function(v) {
+        if (
+          v.split("").some(char => {
+            return char !== "$";
+          }) ||
+          ![1, 2, 3].includes(v.length)
+        ) {
+          return false;
+        }
+        return true;
+      },
+      message: props => `${props.value} is not a valid price range!`
+    }
+  },
+  locationReviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "review"
+    }
+  ]
+});
+
+const Location = mongoose.model("location", LocationSchema);
+
+module.exports = Location;
