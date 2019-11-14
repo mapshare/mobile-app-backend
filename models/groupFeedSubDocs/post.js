4850a0e8b43f604cc3cecf1770e882cdb1f44eb1
coreyjjames <cjames29@myseneca.ca>
2019-11-14 18:42:26 -0500

    update model

--- models/groupFeedSubDocs/post.js
+++ models/groupFeedSubDocs/post.js
@@ -6,16 +6,13 @@ const PostSchema = new Schema({
     postImage: {
         data: Buffer, 
         contentType: String,
-        required: [true, 'Post Image field is required']
     },
     postCaption: {
         type: String,
-        required: [true, 'Post caption field is required']
     },
     postCreatedBy: {
         type: Schema.Types.ObjectId,
         ref: "groupMember",
-        required: [true, 'Member field is required']
     }
 })
 
