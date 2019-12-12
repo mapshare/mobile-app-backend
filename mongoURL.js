2160d1a48e51dba18408bda2b3a3ca1058c184f4
kc-leung <chiu-leung@Hotmail.com>
2019-12-11 19:28:56 -0500

    mongourl

--- mongoURL.js
+++ mongoURL.js
@@ -1,4 +1,4 @@
-const MONGO_URL = 'mongodb://'+ process.env.DB_USER + ':' + process.env.DB_PASS + '@ds127978.mlab.com:27978/pinit';
+const MONGO_URL = 'mongodb://'+ process.env.DB_USER + ':' + process.env.DB_PASS + '@ds237389.mlab.com:37389/pin-it-db';
 //const MONGO_URL = 'mongodb://' + process.env.DB_USER + ':' + process.env.DB_PASS + '@localhost:10016/'+ process.env.DB_NAME;
 
 // temp:Pwd
